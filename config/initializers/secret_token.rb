870f0449ae14f54ffa5c26ed5b488a224d10da8e
NEKOTENI <riverfield.tk@gmail.com>
2014-11-13 22:32:03 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarboCounter::Application.config.secret_key_base = '7e066fac3e1a729e3a2acf93908af590ccb1148dec9ffad9b9eb36357996e2569bc1f88a26efc86656c17847f7ae79c839dc6357fb38b556f970b887901f0c8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarboCounter::Application.config.session_store :cookie_store, key: '_carbo_counter_session'
